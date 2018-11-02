6b557bf4b15b93df756770b189ab10f67b07a1ed
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2018-11-02 12:14:53 +0100

    Add Spring Boot project from https://start.spring.io/
    Use https://start.spring.io/ to generate a project with the web and
    the neo4j dependency. See #35 for more information.

---
+++ mico-core/src/test/java/io/github/ustmico/micocore/MicoCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.ustmico.micocore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicoCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
