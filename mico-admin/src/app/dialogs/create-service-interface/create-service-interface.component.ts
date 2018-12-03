d25d9239812ab593dafcd61d35d71a08fc96a5d7
Heiko Geppert <heiko.geppert@outlook.de>
2018-12-03 17:09:15 +0100

    Add create-service-interface component

---
+++ mico-admin/src/app/dialogs/create-service-interface/create-service-interface.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mico-create-service-interface',
+  templateUrl: './create-service-interface.component.html',
+  styleUrls: ['./create-service-interface.component.css']
+})
+export class CreateServiceInterfaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
