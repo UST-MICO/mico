55fdec31d5509553687716db40c8860c9939df98
JanVetter <25137666+JanVetter@users.noreply.github.com>
2018-12-07 13:10:29 +0100

    Add checks for GitHub Crawler tests

--- mico-core/src/test/java/io/github/ust/mico/core/GitHubCrawlerTests.java
+++ mico-core/src/test/java/io/github/ust/mico/core/GitHubCrawlerTests.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GitHubCrawlerTests {
-    private static final String absoluteUri = "https://api.github.com/repos/octokit/octokit.rb";
+    private static final String REPO_URI = "https://api.github.com/repos/octokit/octokit.rb";
     private static final String RELEASE = "v4.12.0";
 
     @Autowired
@@ -35,7 +35,7 @@ public class GitHubCrawlerTests {
 
         RestTemplateBuilder restTemplate = new RestTemplateBuilder();
         GitHubCrawler crawler = new GitHubCrawler(restTemplate);
-        Service service = crawler.crawlGitHubRepoLatestRelease(absoluteUri);
+        Service service = crawler.crawlGitHubRepoLatestRelease(REPO_URI);
         serviceRepository.save(service);
 
         Service readService = serviceRepository.findByShortNameAndVersion(service.getShortName(), service.getVersion());
@@ -55,7 +55,7 @@ public class GitHubCrawlerTests {
 
         RestTemplateBuilder restTemplate = new RestTemplateBuilder();
         GitHubCrawler crawler = new GitHubCrawler(restTemplate);
-        Service service = crawler.crawlGitHubRepoSpecificRelease(absoluteUri, RELEASE);
+        Service service = crawler.crawlGitHubRepoSpecificRelease(REPO_URI, RELEASE);
         serviceRepository.save(service);
 
         Service readService = serviceRepository.findByShortNameAndVersion(service.getShortName(), service.getVersion());
@@ -75,17 +75,16 @@ public class GitHubCrawlerTests {
 
         RestTemplateBuilder restTemplate = new RestTemplateBuilder();
         GitHubCrawler crawler = new GitHubCrawler(restTemplate);
-        List<Service> serviceList = crawler.crawlGitHubRepoAllReleases(absoluteUri);
+        List<Service> serviceList = crawler.crawlGitHubRepoAllReleases(REPO_URI);
         serviceRepository.saveAll(serviceList);
 
-        //TODO: Add checks
-//        Service readService = serviceRepository.findByShortName(service.getShortName());
-//        assertEquals(service.getShortName(), readService.getShortName());
-//        assertEquals(service.getDescription(), readService.getDescription());
-//        assertEquals(service.getId(), readService.getId());
-//        assertEquals(service.getVersion(), readService.getVersion());
-//        assertEquals(service.getVcsRoot(), readService.getVcsRoot());
-//        assertEquals(service.getName(), readService.getName());
+        Service readService = serviceRepository.findByShortNameAndVersion(serviceList.get(0).getShortName(), serviceList.get(0).getVersion());
+        assertEquals(serviceList.get(0).getShortName(), readService.getShortName());
+        assertEquals(serviceList.get(0).getDescription(), readService.getDescription());
+        assertEquals(serviceList.get(0).getId(), readService.getId());
+        assertEquals(serviceList.get(0).getVersion(), readService.getVersion());
+        assertEquals(serviceList.get(0).getVcsRoot(), readService.getVcsRoot());
+        assertEquals(serviceList.get(0).getName(), readService.getName());
     }
 
     @Test
