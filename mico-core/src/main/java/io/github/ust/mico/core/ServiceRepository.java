cfd296db3fd640ee32288fc0394e57b384a235bb
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2018-12-11 14:38:29 +0100

    Add queries to request the interfaces of a service
    For GET /services/{shortName}/{version}/interfaces and
    GET /services/{shortName}/{version}/interfaces/{serviceInterfaceName}
    it is necessary to request all or a single interface of a specific service.
    This helps to implement #109.

--- mico-core/src/main/java/io/github/ust/mico/core/ServiceRepository.java
+++ mico-core/src/main/java/io/github/ust/mico/core/ServiceRepository.java
@@ -1,6 +1,7 @@
 package io.github.ust.mico.core;
 
 import org.springframework.data.neo4j.annotation.Depth;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -18,6 +19,12 @@ public interface ServiceRepository extends Neo4jRepository<Service, Long> {
     Service findByShortName(String shortName, @Depth int depth);
 
     Optional<Service> findByShortNameAndVersion(String shortName, String version);
+
+    @Query("MATCH (service:Service)-[:SERVICE_INTERFACES]->(interface:ServiceInterface) WHERE service.shortName = {shortName} AND service.version = {version} return interface")
+    List<ServiceInterface> findInterfacesOfService(@Param("shortName") String shortName,@Param("version")  String version);
+
+    @Query("MATCH (service:Service)-[:SERVICE_INTERFACES]->(interface:ServiceInterface) WHERE service.shortName = {shortName} AND service.version = {version} AND interface.serviceInterfaceName = {serviceInterfaceName} return interface")
+    Optional<ServiceInterface> findInterfaceOfServiceByName(@Param("serviceInterfaceName") String serviceInterfaceName, @Param("shortName") String shortName,@Param("version")  String version);
 
     @Override
     List<Service> findAll();
