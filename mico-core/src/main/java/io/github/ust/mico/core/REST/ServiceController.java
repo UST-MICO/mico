8f26e0ff6a95a5db06765a3838676519964f1766
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2018-12-11 14:44:24 +0100

    Add endpoints to GET a single or all service interfaces
    Add GET /services/{shortName}/{version}/interfaces and
    GET /services/{shortName}/{version}/interfaces/{serviceInterfaceName}
    as required by #109.

--- mico-core/src/main/java/io/github/ust/mico/core/REST/ServiceController.java
+++ mico-core/src/main/java/io/github/ust/mico/core/REST/ServiceController.java
@@ -1,6 +1,7 @@
 package io.github.ust.mico.core.REST;
 
 import io.github.ust.mico.core.Service;
+import io.github.ust.mico.core.ServiceInterface;
 import io.github.ust.mico.core.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -28,6 +29,8 @@ public class ServiceController {
     public static final String PATH_VARIABLE_SHORT_NAME = "shortName";
     public static final String PATH_VARIABLE_VERSION = "version";
     public static final String PATH_VARIABLE_ID = "id";
+    private static final String PATH_VARIABLE_SERVICE_INTERFACE_NAME = "serviceInterfaceName";
+    private static final String PATH_PART_INTERFACES = "/interfaces";
 
     @Autowired
     private ServiceRepository serviceRepository;
@@ -58,10 +61,31 @@ public class ServiceController {
                 new Resources<>(serviceResources,
                         linkTo(methodOn(ServiceController.class).getVersionsOfService(shortName)).withSelfRel()));
     }
-    
+
+
+    //GET| /services/{shortName}/{version}/interface
+    @GetMapping("/{" + PATH_VARIABLE_SHORT_NAME + "}/{" + PATH_VARIABLE_VERSION + "}" + PATH_PART_INTERFACES)
+    public ResponseEntity<Resources<Resource<ServiceInterface>>> getInterfacesOfService(@PathVariable(PATH_VARIABLE_SHORT_NAME) String shortName,
+                                                                                        @PathVariable(PATH_VARIABLE_VERSION) String version) {
+        List<ServiceInterface> serviceInterfaces = serviceRepository.findInterfacesOfService(shortName, version);
+        List<Resource<ServiceInterface>> serviceInterfaceResources = serviceInterfaces.stream().map(
+                serviceInterface -> new Resource<>(serviceInterface, getServiceInterfaceLinks(serviceInterface, shortName, version))
+        ).collect(Collectors.toList());
+        return ResponseEntity.ok(new Resources<>(serviceInterfaceResources, linkTo(methodOn(ServiceController.class).getInterfacesOfService(shortName, version)).withSelfRel()));
+    }
+
+    @GetMapping("/{" + PATH_VARIABLE_SHORT_NAME + "}/{" + PATH_VARIABLE_VERSION + "}" + PATH_PART_INTERFACES + "/{" + PATH_VARIABLE_SERVICE_INTERFACE_NAME + "}")
+    public ResponseEntity<Resource<ServiceInterface>> getInterfaceByName(@PathVariable(PATH_VARIABLE_SHORT_NAME) String shortName,
+                                                                         @PathVariable(PATH_VARIABLE_VERSION) String version,
+                                                                         @PathVariable(PATH_VARIABLE_SERVICE_INTERFACE_NAME) String serviceInterfaceName) {
+        Optional<ServiceInterface> serviceInterfaceOptional = serviceRepository.findInterfaceOfServiceByName(serviceInterfaceName, shortName, version);
+        return serviceInterfaceOptional.map(serviceInterface ->
+                new Resource<>(serviceInterface, getServiceInterfaceLinks(serviceInterface, shortName, version))).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
     //TODO: Ambiguous endpoint with /services/shortName
     //@GetMapping("/{" + PATH_VARIABLE_ID + "}/")
-    public ResponseEntity<Resource<Service>> getServiceById(@PathVariable(PATH_VARIABLE_ID) Long id){
+    public ResponseEntity<Resource<Service>> getServiceById(@PathVariable(PATH_VARIABLE_ID) Long id) {
         Optional<Service> serviceOpt = serviceRepository.findById(id);
 
         return serviceOpt.map(service -> new Resource<>(service, getServiceLinks(service)))
@@ -73,6 +97,12 @@ public class ServiceController {
                 .collect(Collectors.toList());
     }
 
+    private Iterable<Link> getServiceInterfaceLinks(ServiceInterface serviceInterface, String shortName, String version) {
+        LinkedList<Link> links = new LinkedList<>();
+        links.add(linkTo(methodOn(ServiceController.class).getInterfaceByName(shortName, version, serviceInterface.getServiceInterfaceName())).withSelfRel());
+        links.add(linkTo(methodOn(ServiceController.class).getInterfacesOfService(shortName, version)).withRel("interfaces"));
+        return links;
+    }
 
     private Iterable<Link> getServiceLinks(Service service) {
         LinkedList<Link> links = new LinkedList<>();
