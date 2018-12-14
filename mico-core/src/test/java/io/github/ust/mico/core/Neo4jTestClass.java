ef84fdb5d155dcf7da6f53ef0eca87dcce6a1af7
JanVetter <25137666+JanVetter@users.noreply.github.com>
2018-12-14 15:23:14 +0100

    Initial

---
+++ mico-core/src/test/java/io/github/ust/mico/core/Neo4jTestClass.java
@@ -0,0 +1,41 @@
+package io.github.ust.mico.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.neo4j.ogm.session.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+abstract class Neo4jTestClass {
+
+    @Autowired
+    ApplicationRepository applicationRepository;
+
+    @Autowired
+    DependsOnRepository dependsOnRepository;
+
+    @Autowired
+    ServiceInterfaceRepository serviceInterfaceRepository;
+
+    @Autowired
+    ServiceRepository serviceRepository;
+
+    @Autowired
+    Session session;
+
+    @Before
+    public void setUp() {
+        //TODO: Implementation
+    }
+
+    @After
+    public void tearDown() {
+        session.purgeDatabase();
+    }
+}
