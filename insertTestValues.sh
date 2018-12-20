1d59fc5028a72ce1c4861bdf0aeec296c74dae1d
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2018-12-20 10:34:13 +0100

    Add test values to init script
    In addition to the setup script from #137,
    this script lets a developer insert test values into the database.

--- insertTestValues.sh
+++ insertTestValues.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -X GET "http://localhost:8080/error" -H  "accept: text/html"
+curl -X POST "http://localhost:8080/services" -H  "accept: application/hal+json" -H  "Content-Type: application/json" -d "{  \"shortName\": \"TestService\",  \"version\": \"1.0\"}"
+curl -X POST "http://localhost:8080/services" -H  "accept: application/hal+json" -H  "Content-Type: application/json" -d "{  \"contact\": \"max@musterman.de\",  \"description\": \"This is a test service\",  \"dockerImageName\": \"TestImage\",  \"dockerImageUri\": \"http://...\",  \"name\": \"Test Service TWO\",  \"owner\": \"Max Musterman\",  \"shortName\": \"testService2\",  \"vcsRoot\": \"http://....\",  \"version\": \"1.2\"}"
