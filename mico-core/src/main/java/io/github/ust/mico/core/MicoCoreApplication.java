93d962ab265ee93b214f71e181fca98c6524e72e
JanVetter <25137666+JanVetter@users.noreply.github.com>
2018-12-21 13:44:18 +0100

    Reformat code

--- mico-core/src/main/java/io/github/ust/mico/core/MicoCoreApplication.java
+++ mico-core/src/main/java/io/github/ust/mico/core/MicoCoreApplication.java
@@ -20,10 +20,9 @@
  *****************************************************************/
 
 package io.github.ust.mico.core;
-import org.neo4j.ogm.session.SessionFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
