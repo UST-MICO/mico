f4d57c6c9adf97981fdb9f7f392d0f65e15099f1
Heiko Geppert <heiko.geppert@outlook.de>
2018-12-21 16:53:14 +0100

    Add filtering by shortName in service-list

--- mico-admin/src/app/service-list/service-list.component.ts
+++ mico-admin/src/app/service-list/service-list.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { ApiService } from '../api/api.service';
 import { ApiObject } from '../api/apiobject';
+import { Subscription } from 'rxjs';
+import { from } from 'rxjs';
+import { groupBy, mergeMap, toArray } from 'rxjs/operators';
 
 @Component({
     selector: 'mico-service-list',
     templateUrl: './service-list.component.html',
     styleUrls: ['./service-list.component.css']
 })
-export class ServiceListComponent implements OnInit {
+export class ServiceListComponent implements OnInit, OnDestroy {
+
+    private subServices: Subscription;
 
     constructor(
         private apiService: ApiService
@@ -22,10 +27,25 @@ export class ServiceListComponent implements OnInit {
     ngOnInit() {
     }
 
-    getServices(): void {
-        this.apiService.getServices()
-            .subscribe(val => {
-                this.services = val;
-            });
+    ngOnDestroy() {
+        if (this.subServices != null) {
+            this.subServices.unsubscribe();
+        }
     }
+
+    getServices(): void {
+
+        // group services by shortName
+        const tempServices: ApiObject[] = [];
+        this.subServices = this.apiService.getServices()
+            .subscribe(val => {
+                from(val).pipe(groupBy(service => service.shortName), mergeMap(group => group.pipe(toArray())))
+                    .subscribe(group => {
+                        tempServices.push(group[0]);
+                    });
+                this.services = tempServices;
+            });
+
+    }
+
 }
