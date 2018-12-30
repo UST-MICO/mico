a09f1331c77b6d8652452b541de6a982f5d92406
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2018-12-30 17:31:44 +0100

    Add CORS support

---
+++ mico-core/src/main/java/io/github/ust/mico/core/REST/WebConfig.java
@@ -0,0 +1,19 @@
+package io.github.ust.mico.core.REST;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors-policy.allowed-origins}")
+    String[] allowedOrigins;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(allowedOrigins);
+    }
+}
