61331e08d0481d6df193941cf69cd263380b081c
Heiko Geppert <heiko.geppert@outlook.de>
2019-01-07 17:13:56 +0100

    Add first part of service version handling in urls for service-details view

--- mico-admin/src/app/service-detail/service-detail.component.ts
+++ mico-admin/src/app/service-detail/service-detail.component.ts
@@ -34,7 +34,8 @@ export class ServiceDetailComponent implements OnInit, OnDestroy {
         this.subParam = this.route.params.subscribe(params => {
 
             this.shortName = params['shortName'];
-            this.update(this.shortName);
+            const version = params['version'];
+            this.update(this.shortName, version);
         });
     }
 
@@ -47,7 +48,7 @@ export class ServiceDetailComponent implements OnInit, OnDestroy {
         }
     }
 
-    update(shortName) {
+    update(shortName, givenVersion) {
         if (shortName === this.id) {
             return;
         }
@@ -57,23 +58,53 @@ export class ServiceDetailComponent implements OnInit, OnDestroy {
         }
 
         // get latest version
-        let version = '0';
+
+        // TODO change url path according to the current version
+
         this.subService = this.apiService.getServiceVersions(shortName)
             .subscribe(serviceVersions => {
 
                 this.versions = serviceVersions;
 
-                serviceVersions.forEach(element => {
+                if (givenVersion == null) {
+                    this.setLatestVersion(serviceVersions);
+                } else {
+                    let found = false;
+                    found = serviceVersions.forEach(element => {
 
-                    // TODO implement comparison for semantic versioning
-                    if (element.version > version) {
-                        version = element.version;
-                        this.selectedVersion = element.version;
-                        this.service = element;
+                        if (element.version === givenVersion) {
+                            this.selectedVersion = givenVersion;
+                            this.service = element;
+                            return true;
+                        }
+                    });
 
+                    if (!found) {
+                        // given version was not found in the versions list, take latest instead
+                        this.setLatestVersion(serviceVersions);
                     }
-                });
+                }
+
             });
+    }
+
+    /**
+     * takes a list of services and sets this.service to the service with the latest version
+     * this.version is set accoringly
+     */
+    setLatestVersion(list) {
+        list.forEach(element => {
+
+            let version = '0';
+
+            // TODO implement comparison for semantic versioning
+            if (element.version > version) {
+                version = element.version;
+                this.selectedVersion = element.version;
+                this.service = element;
+
+            }
+        });
     }
 
 }
