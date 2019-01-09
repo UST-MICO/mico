88ba4a2f5cece56317a1daa65bd4a1f40d8d89e9
JanVetter <25137666+JanVetter@users.noreply.github.com>
2019-01-09 13:12:42 +0100

    Reformat code layout

--- mico-core/src/test/java/io/github/ust/mico/core/ApplicationControllerTest.java
+++ mico-core/src/test/java/io/github/ust/mico/core/ApplicationControllerTest.java
@@ -21,10 +21,10 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ApplicationController.class)
@@ -51,38 +51,38 @@ public class ApplicationControllerTest {
     @Test
     public void getAllApplications() throws Exception {
         given(applicationRepository.findAll()).willReturn(
-                Arrays.asList(
-                        new Application("ShortName1", "1.0.1"),
-                        new Application("ShortName1", "1.0.0"),
-                        new Application("ShortName2", "1.0.0")));
+            Arrays.asList(
+                new Application("ShortName1", "1.0.1"),
+                new Application("ShortName1", "1.0.0"),
+                new Application("ShortName2", "1.0.0")));
 
         mvc.perform(get("/applications").accept(MediaTypes.HAL_JSON_UTF8_VALUE))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$._embedded.applicationList[*]", hasSize(3)))
-                .andExpect(jsonPath("$._embedded.applicationList[?(@.shortName =='ShortName1' && @.version == '1.0.0' )]", hasSize(1)))
-                .andExpect(jsonPath("$._embedded.applicationList[?(@.shortName =='ShortName1' && @.version == '1.0.1' )]", hasSize(1)))
-                .andExpect(jsonPath("$._embedded.applicationList[?(@.shortName =='ShortName2' && @.version == '1.0.0' )]", hasSize(1)))
-                .andExpect(jsonPath(JSON_PATH_LINKS_SECTION + "self.href", is("http://localhost/applications")))
-                .andReturn();
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$._embedded.applicationList[*]", hasSize(3)))
+            .andExpect(jsonPath("$._embedded.applicationList[?(@.shortName =='ShortName1' && @.version == '1.0.0' )]", hasSize(1)))
+            .andExpect(jsonPath("$._embedded.applicationList[?(@.shortName =='ShortName1' && @.version == '1.0.1' )]", hasSize(1)))
+            .andExpect(jsonPath("$._embedded.applicationList[?(@.shortName =='ShortName2' && @.version == '1.0.0' )]", hasSize(1)))
+            .andExpect(jsonPath(JSON_PATH_LINKS_SECTION + "self.href", is("http://localhost/applications")))
+            .andReturn();
 
     }
 
     @Test
     public void getApplicationByShortNameAndVersion() throws Exception {
-        given(applicationRepository.findByShortNameAndVersion("ApplicationShortName","1.1.0")).willReturn(
-                Optional.of(new Application("ApplicationShortName","1.1.0")));
+        given(applicationRepository.findByShortNameAndVersion("ApplicationShortName", "1.1.0")).willReturn(
+            Optional.of(new Application("ApplicationShortName", "1.1.0")));
 
         mvc.perform(get("/applications/ApplicationShortName/1.1.0").accept(MediaTypes.HAL_JSON_VALUE))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$.shortName", is("ApplicationShortName")))
-                .andExpect(jsonPath("$.version", is("1.1.0")))
-                .andExpect(jsonPath(JSON_PATH_LINKS_SECTION + SELF_HREF, is("http://localhost/applications/ApplicationShortName/1.1.0")))
-                .andExpect(jsonPath(JSON_PATH_LINKS_SECTION + "applications.href", is("http://localhost/applications")))
-                .andReturn();
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.shortName", is("ApplicationShortName")))
+            .andExpect(jsonPath("$.version", is("1.1.0")))
+            .andExpect(jsonPath(JSON_PATH_LINKS_SECTION + SELF_HREF, is("http://localhost/applications/ApplicationShortName/1.1.0")))
+            .andExpect(jsonPath(JSON_PATH_LINKS_SECTION + "applications.href", is("http://localhost/applications")))
+            .andReturn();
     }
 
     @Test
@@ -93,9 +93,9 @@ public class ApplicationControllerTest {
         given(applicationRepository.save(any(Application.class))).willReturn(application);
 
         final ResultActions result = mvc.perform(post(BASE_PATH)
-                .content(mapper.writeValueAsBytes(application))
-                .contentType(MediaTypes.HAL_JSON_UTF8_VALUE))
-                .andDo(print());
+            .content(mapper.writeValueAsBytes(application))
+            .contentType(MediaTypes.HAL_JSON_UTF8_VALUE))
+            .andDo(print());
 
         result.andExpect(status().isCreated());
     }
