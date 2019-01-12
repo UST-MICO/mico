6a3d9b011688658f958f9db0b3770a9f0dd5d86c
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-01-12 15:14:45 +0100

    Add CORS configuration bean

--- mico-core/src/main/java/io/github/ust/mico/core/WebConfig.java
+++ mico-core/src/main/java/io/github/ust/mico/core/WebConfig.java
@@ -1,19 +1,24 @@
-package io.github.ust.mico.core.REST;
+package io.github.ust.mico.core;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
     @Value("${cors-policy.allowed-origins}")
     String[] allowedOrigins;
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(allowedOrigins);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowedOrigins);
+            }
+        };
     }
 }
