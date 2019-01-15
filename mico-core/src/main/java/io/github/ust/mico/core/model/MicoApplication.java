8eeeadacd36ad99a030b8b056c8e0d67ea5bf909
JanVetter <25137666+JanVetter@users.noreply.github.com>
2019-01-15 11:42:15 +0100

    Update MicoApplication.java

--- mico-core/src/main/java/io/github/ust/mico/core/model/MicoApplication.java
+++ mico-core/src/main/java/io/github/ust/mico/core/model/MicoApplication.java
@@ -1,47 +1,37 @@
 package io.github.ust.mico.core.model;
 
-import java.util.List;
-
-import org.neo4j.ogm.annotation.GeneratedValue;
-import org.neo4j.ogm.annotation.Id;
-import org.neo4j.ogm.annotation.NodeEntity;
-
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Singular;
+import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
+
+import java.util.List;
 
 /**
  * Represents an application as a set of {@link MicoService}s
  * in the context of MICO.
  */
 @Data
-@AllArgsConstructor
 @Builder
+//@RequiredArgsConstructor
+//@AllArgsConstructor
 @NodeEntity
-public class MicoApplication {
-
-    @Id
-    @GeneratedValue
-    private final long id;
-
+public class MicoApplication extends MicoService {
 
     // ----------------------
     // -> Required fields ---
     // ----------------------
 
-    // TODO: @Jakob -> Do we want to link the DB object instead of the ID?
     // The ids of the services this application is composed of.
     @ApiModelProperty(required = true)
     @Singular
-    @Relationship //TODO: @Jan -> add more info / annotation needed?
-    private final List<Long> services;
+    @Relationship
+    private final List<MicoService> services;
 
     // The information necessary for deploying this application.
     @ApiModelProperty(required = true)
-    @Relationship  //TODO: @Jan -> add more info / annotation needed?
     private final MicoApplicationDeploymentInfo deploymentInfo;
 
 }
