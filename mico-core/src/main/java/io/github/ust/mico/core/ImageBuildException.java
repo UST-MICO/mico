74a1bb29c27174eafd3ae61d1ed344301a2863aa
David Kopp <david@kopp-heim.de>
2019-01-21 15:23:59 +0100

    Add waiting for image build

---
+++ mico-core/src/main/java/io/github/ust/mico/core/ImageBuildException.java
@@ -0,0 +1,10 @@
+package io.github.ust.mico.core;
+
+public class ImageBuildException extends Exception {
+    public ImageBuildException() {
+    }
+
+    public ImageBuildException(String message) {
+        super(message);
+    }
+}
