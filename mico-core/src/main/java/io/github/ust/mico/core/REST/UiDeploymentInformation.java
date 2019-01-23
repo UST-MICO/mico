a52c7d4198198706277a962e79ab8e35cbae2743
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-01-23 22:11:12 +0100

    Add a model to display deployment info in the UI

---
+++ mico-core/src/main/java/io/github/ust/mico/core/REST/UiDeploymentInformation.java
@@ -0,0 +1,21 @@
+package io.github.ust.mico.core.REST;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UiDeploymentInformation {
+
+    int requestedReplicas;
+    int availableReplicas;
+    List<UiExternalMicoInterfaceInformation> interfacesInformation;
+    List<UiPodInfo> podInfo;
+
+}
