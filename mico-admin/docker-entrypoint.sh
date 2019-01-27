70a160dc0ab24ef3970435972e1ab59b9f7a4ddb
Joachim Spalink <j.spalink@gmx.de>
2019-01-27 19:10:17 +0100

    changed shell for alpine linux

--- mico-admin/docker-entrypoint.sh
+++ mico-admin/docker-entrypoint.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/ash
 set -e
 
-echo "starting" >&2
-envsubst '${MICO_REST_API}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
-echo "replaced" >&2
+echo "MICO Rest Api Url" ${MICO_REST_API}
+envsubst ${MICO_REST_API} < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
 exec "$@"
