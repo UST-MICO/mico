f7d70f8b07e38c46526b051eb6c79a494c5b4c69
Jakob Benz <jakob.michael.benz@gmail.com>
2019-01-27 15:20:31 +0100

    Add serial version uids where necessary.

--- mico-core/src/main/java/io/github/ust/mico/core/imagebuilder/BuildList.java
+++ mico-core/src/main/java/io/github/ust/mico/core/imagebuilder/BuildList.java
@@ -4,4 +4,7 @@ import io.fabric8.kubernetes.client.CustomResourceList;
 import io.github.ust.mico.core.imagebuilder.buildtypes.Build;
 
 public class BuildList extends CustomResourceList<Build> {
+
+    private static final long serialVersionUID = 3524519220752406464L;
+
 }
