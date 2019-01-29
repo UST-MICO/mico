60bdd7b58ac1fa453661f98f96787acc976fdabc
Joachim Spalink <Jojo134@users.noreply.github.com>
2019-01-29 12:32:38 +0100

    add nameserver var (#327)
    * adding nameserver to mico-admin for nginx to resolve

--- mico-admin/docker-entrypoint.sh
+++ mico-admin/docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/ash
 set -eu
 
-echo "MICO Rest Api Url" ${MICO_REST_API}
+echo "MICO Rest Api Url is: ${MICO_REST_API}""
 envsubst '${MICO_REST_API}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
+
+if [ "$NAMESERVER" == "" ]; then
+    export NAMESERVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
+fi
+echo "Nameserver is: ${NAMESERVER}"
+envsubst '${NAMESERVER}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
 
 exec "$@"
