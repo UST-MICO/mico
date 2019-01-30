94b6cc6342732fcc4f54bdd52921d1432444322e
Joachim Spalink <j.spalink@gmx.de>
2019-01-30 08:14:45 +0100

    env var replacing with 1 call

--- mico-admin/docker-entrypoint.sh
+++ mico-admin/docker-entrypoint.sh
@@ -1,15 +1,18 @@
-#!/bin/ash
-set -eu
+#!/bin/sh
+set -e
 
 echo "MICO Rest Api Url is: ${MICO_REST_API}"
-envsubst '${MICO_REST_API}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
 
-if [ "${NAMESERVER}" == "" ]; then
+if [ "$NAMESERVER" == "" ]
+then
     export NAMESERVER=$(awk '/^nameserver/{print $2}' /etc/resolv.conf)
 fi
 
-echo "Nameserver is:" ${NAMESERVER}
+echo "Nameserver is:" $NAMESERVER
 
-envsubst '${NAMESERVER}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
+envsubst '$NAMESERVER ${MICO_REST_API}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
+echo "done replacing"
+
+cat /etc/nginx/nginx.conf
 
 exec "$@"
