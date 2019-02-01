5847fc81bd58fa66cb83a50e9ac9308b2a5a6d7f
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-02-01 17:52:47 +0100

    Add DELETE,PATCH and PUT to CORS config

--- mico-core/src/main/java/io/github/ust/mico/core/WebConfig.java
+++ mico-core/src/main/java/io/github/ust/mico/core/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -26,6 +27,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public FilterRegistrationBean<CorsFilter> corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod(HttpMethod.DELETE);
+        config.addAllowedMethod(HttpMethod.PATCH);
+        config.addAllowedMethod(HttpMethod.PUT);
         config.setAllowCredentials(false);
         config.setAllowedOrigins(Arrays.asList(allowedOrigins));
         source.registerCorsConfiguration("/**", config);
