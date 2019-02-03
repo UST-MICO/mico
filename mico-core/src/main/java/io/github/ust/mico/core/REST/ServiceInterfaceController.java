2d2ee63f93b8232ee98345aa460314e6b3808157
David Kopp <david@kopp-heim.de>
2019-02-03 01:31:44 +0100

    Fix endpoint GET public IP

--- mico-core/src/main/java/io/github/ust/mico/core/REST/ServiceInterfaceController.java
+++ mico-core/src/main/java/io/github/ust/mico/core/REST/ServiceInterfaceController.java
@@ -1,15 +1,12 @@
 package io.github.ust.mico.core.REST;
 
-import static io.github.ust.mico.core.REST.ServiceController.PATH_VARIABLE_SHORT_NAME;
-import static io.github.ust.mico.core.REST.ServiceController.PATH_VARIABLE_VERSION;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
+import io.fabric8.kubernetes.api.model.Service;
+import io.github.ust.mico.core.ClusterAwarenessFabric8;
+import io.github.ust.mico.core.MicoKubernetesConfig;
+import io.github.ust.mico.core.model.MicoService;
+import io.github.ust.mico.core.model.MicoServiceInterface;
+import io.github.ust.mico.core.persistence.MicoServiceRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
@@ -17,21 +14,22 @@ import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import io.github.ust.mico.core.ClusterAwarenessFabric8;
-import io.github.ust.mico.core.MicoKubernetesConfig;
-import io.github.ust.mico.core.model.MicoService;
-import io.github.ust.mico.core.model.MicoServiceInterface;
-import io.github.ust.mico.core.persistence.MicoServiceRepository;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import static io.github.ust.mico.core.REST.ServiceController.PATH_VARIABLE_SHORT_NAME;
+import static io.github.ust.mico.core.REST.ServiceController.PATH_VARIABLE_VERSION;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.http.ResponseEntity.ok;
+
+@Slf4j
 @RestController
 @RequestMapping(value = "/services", produces = MediaTypes.HAL_JSON_VALUE)
 public class ServiceInterfaceController {
@@ -39,53 +37,67 @@ public class ServiceInterfaceController {
     private static final String PATH_VARIABLE_SERVICE_INTERFACE_NAME = "serviceInterfaceName";
     private static final String PATH_PART_INTERFACES = "interfaces";
     private static final String PATH_PART_PUBLIC_IP = "publicIP";
-    private static final String SERVICE_INTERFACE_PATH = "/{" + PATH_VARIABLE_SHORT_NAME + "}/{" + PATH_VARIABLE_VERSION + "}/" + PATH_PART_INTERFACES + "/";
-    private static final String SERVICE_INTERFACE_PUBLIC_IP_PATH = SERVICE_INTERFACE_PATH + "{" + PATH_VARIABLE_SERVICE_INTERFACE_NAME + "}/" + PATH_PART_PUBLIC_IP + "/";
+    private static final String SERVICE_INTERFACE_PATH = "/{" + PATH_VARIABLE_SHORT_NAME + "}/{" + PATH_VARIABLE_VERSION + "}/" + PATH_PART_INTERFACES;
+    private static final String SERVICE_INTERFACE_PUBLIC_IP_PATH = SERVICE_INTERFACE_PATH + "/{" + PATH_VARIABLE_SERVICE_INTERFACE_NAME + "}/" + PATH_PART_PUBLIC_IP;
 
     @Autowired
     private MicoServiceRepository serviceRepository;
-    
+
     @Autowired
     private ClusterAwarenessFabric8 cluster;
-    
+
     @Autowired
     private MicoKubernetesConfig kubernetesConfig;
 
     @GetMapping(SERVICE_INTERFACE_PATH)
     public ResponseEntity<Resources<Resource<MicoServiceInterface>>> getInterfacesOfService(@PathVariable(PATH_VARIABLE_SHORT_NAME) String shortName,
-                                                                                        @PathVariable(PATH_VARIABLE_VERSION) String version) {
+                                                                                            @PathVariable(PATH_VARIABLE_VERSION) String version) {
         List<MicoServiceInterface> serviceInterfaces = serviceRepository.findInterfacesOfService(shortName, version);
         List<Resource<MicoServiceInterface>> serviceInterfaceResources = serviceInterfaces.stream().map(
             serviceInterface -> new Resource<>(serviceInterface, getServiceInterfaceLinks(serviceInterface, shortName, version))
         ).collect(Collectors.toList());
-        return ResponseEntity.ok(new Resources<>(serviceInterfaceResources, linkTo(methodOn(ServiceInterfaceController.class).getInterfacesOfService(shortName, version)).withSelfRel()));
+        return ok(new Resources<>(serviceInterfaceResources, linkTo(methodOn(ServiceInterfaceController.class).getInterfacesOfService(shortName, version)).withSelfRel()));
     }
 
-    @GetMapping(SERVICE_INTERFACE_PATH + "{" + PATH_VARIABLE_SERVICE_INTERFACE_NAME + "}")
+    @GetMapping(SERVICE_INTERFACE_PATH + "/{" + PATH_VARIABLE_SERVICE_INTERFACE_NAME + "}")
     public ResponseEntity<Resource<MicoServiceInterface>> getInterfaceByName(@PathVariable(PATH_VARIABLE_SHORT_NAME) String shortName,
-                                                                         @PathVariable(PATH_VARIABLE_VERSION) String version,
-                                                                         @PathVariable(PATH_VARIABLE_SERVICE_INTERFACE_NAME) String serviceInterfaceName) {
+                                                                             @PathVariable(PATH_VARIABLE_VERSION) String version,
+                                                                             @PathVariable(PATH_VARIABLE_SERVICE_INTERFACE_NAME) String serviceInterfaceName) {
         Optional<MicoServiceInterface> serviceInterfaceOptional = serviceRepository.findInterfaceOfServiceByName(serviceInterfaceName, shortName, version);
         return serviceInterfaceOptional.map(serviceInterface ->
             new Resource<>(serviceInterface, getServiceInterfaceLinks(serviceInterface, shortName, version))).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
-    
+
     @GetMapping(SERVICE_INTERFACE_PUBLIC_IP_PATH)
-    public ResponseEntity<Resources<Resource<String>>> getInterfacePublicIpByName(@PathVariable(PATH_VARIABLE_SHORT_NAME) String shortName,
-                                                                         @PathVariable(PATH_VARIABLE_VERSION) String version,
-                                                                         @PathVariable(PATH_VARIABLE_SERVICE_INTERFACE_NAME) String serviceInterfaceName) {
-        List<String> publicIps = cluster.getService(shortName, kubernetesConfig.getNamespaceMicoWorkspace()).getSpec().getExternalIPs();
-//        List<String> publicIps = new ArrayList<String>();
-//        publicIps.add("51.136.51.59");
-        List<Resource<String>> publicIpResources = publicIps.stream().map(publicIp -> new Resource<>(publicIp)).collect(Collectors.toList());
-        return ResponseEntity.ok().body(new Resources<>(publicIpResources));
+    public ResponseEntity<List<String>> getInterfacePublicIpByName(@PathVariable(PATH_VARIABLE_SHORT_NAME) String shortName,
+                                                                   @PathVariable(PATH_VARIABLE_VERSION) String version,
+                                                                   @PathVariable(PATH_VARIABLE_SERVICE_INTERFACE_NAME) String serviceInterfaceName) {
+        Optional<MicoServiceInterface> serviceInterfaceOptional = serviceRepository.findInterfaceOfServiceByName(serviceInterfaceName, shortName, version);
+        if (!serviceInterfaceOptional.isPresent()) {
+            log.debug("Service interface with name '{}' of MicoService '{}' in version '{}' not found",
+                serviceInterfaceName, shortName, version);
+            return ResponseEntity.notFound().build();
+        }
+
+        // TODO Change naming of Kubernetes service
+        Service service = cluster.getService(serviceInterfaceName, kubernetesConfig.getNamespaceMicoWorkspace());
+        if (service == null) {
+            log.debug("Kubernetes service with name '{}' in namespace '{}' not found",
+                serviceInterfaceName, kubernetesConfig.getNamespaceMicoWorkspace());
+            return ResponseEntity.notFound().build();
+        }
+
+        List<String> publicIps = service.getSpec().getExternalIPs();
+        log.debug("Service interface with name '{}' of MicoService '{}' in version '{}' has external IPs: {}",
+            serviceInterfaceName, shortName, version, publicIps);
+        return ok().body(new ArrayList<>(publicIps));
     }
 
-    @DeleteMapping(SERVICE_INTERFACE_PATH + "{" + PATH_VARIABLE_SERVICE_INTERFACE_NAME + "}")
+    @DeleteMapping(SERVICE_INTERFACE_PATH + "/{" + PATH_VARIABLE_SERVICE_INTERFACE_NAME + "}")
     public void deleteServiceInterface(@PathVariable(PATH_VARIABLE_SHORT_NAME) String shortName,
-                        @PathVariable(PATH_VARIABLE_VERSION) String version,
-                        @PathVariable(PATH_VARIABLE_SERVICE_INTERFACE_NAME) String serviceInterfaceName) {
-        serviceRepository.deleteInterfaceOfServiceByName(serviceInterfaceName,shortName,version);
+                                       @PathVariable(PATH_VARIABLE_VERSION) String version,
+                                       @PathVariable(PATH_VARIABLE_SERVICE_INTERFACE_NAME) String serviceInterfaceName) {
+        serviceRepository.deleteInterfaceOfServiceByName(serviceInterfaceName, shortName, version);
     }
 
 
@@ -100,8 +112,8 @@ public class ServiceInterfaceController {
      */
     @PostMapping(SERVICE_INTERFACE_PATH)
     public ResponseEntity<Resource<MicoServiceInterface>> createServiceInterface(@PathVariable(PATH_VARIABLE_SHORT_NAME) String shortName,
-                                                                             @PathVariable(PATH_VARIABLE_VERSION) String version,
-                                                                             @RequestBody MicoServiceInterface serviceInterface) {
+                                                                                 @PathVariable(PATH_VARIABLE_VERSION) String version,
+                                                                                 @RequestBody MicoServiceInterface serviceInterface) {
         Optional<MicoService> serviceOptional = serviceRepository.findByShortNameAndVersion(shortName, version);
         if (serviceOptional.isPresent()) {
             MicoService service = serviceOptional.get();
@@ -120,7 +132,7 @@ public class ServiceInterfaceController {
     }
 
     private boolean serviceInterfaceExists(@RequestBody MicoServiceInterface serviceInterface, MicoService service) {
-        if(service.getServiceInterfaces() == null){
+        if (service.getServiceInterfaces() == null) {
             return false;
         }
         return service.getServiceInterfaces().stream().anyMatch(existingServiceInterface -> existingServiceInterface.getServiceInterfaceName().equals(serviceInterface.getServiceInterfaceName()));
--- mico-core/src/test/java/io/github/ust/mico/core/ServiceInterfaceControllerTests.java
+++ mico-core/src/test/java/io/github/ust/mico/core/ServiceInterfaceControllerTests.java
@@ -1,6 +1,8 @@
 package io.github.ust.mico.core;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
 import io.github.ust.mico.core.REST.ServiceInterfaceController;
 import io.github.ust.mico.core.model.MicoPortType;
 import io.github.ust.mico.core.model.MicoService;
@@ -20,10 +22,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -49,7 +49,7 @@ public class ServiceInterfaceControllerTests {
     private static final String JSON_PATH_LINKS_SECTION = buildPath(ROOT, LINKS);
     private static final String SELF_HREF = buildPath(JSON_PATH_LINKS_SECTION, SELF, HREF);
     private static final String INTERFACES_HREF = buildPath(JSON_PATH_LINKS_SECTION, "interfaces", HREF);
-    private static final String INTERFACE_NAME = "Interface Test";
+    private static final String INTERFACE_NAME = "interface-name";
     private static final int INTERFACE_PORT = 1024;
     private static final MicoPortType INTERFACE_PORT_TYPE = MicoPortType.TCP;
     private static final int INTERFACE_TARGET_PORT = 1025;
@@ -57,20 +57,21 @@ public class ServiceInterfaceControllerTests {
     private static final String INTERFACE_PUBLIC_DNS = "DNS";
     private static final String SERVICES_HREF = buildPath(ROOT, LINKS, "service", HREF);
     private static final String SERVICE_URL = "/services/" + SHORT_NAME + "/" + VERSION;
-    private static final String INTERFACES_URL = SERVICE_URL + "/interfaces/";
-    
+    private static final String INTERFACES_URL = SERVICE_URL + "/interfaces";
+    private static final String PATH_PART_PUBLIC_IP = "publicIP";
+
     @Autowired
     private MockMvc mvc;
-    
+
     @MockBean
     private MicoServiceRepository serviceRepository;
-    
+
     @MockBean
     private ClusterAwarenessFabric8 cluster;
-    
+
     @MockBean
     private MicoKubernetesConfig kubernetesConfig;
-    
+
     @Autowired
     private ObjectMapper mapper;
 
@@ -124,7 +125,7 @@ public class ServiceInterfaceControllerTests {
         given(serviceRepository.findInterfaceOfServiceByName(serviceInterface.getServiceInterfaceName(), SHORT_NAME, VERSION)).willReturn(
             Optional.of(serviceInterface));
 
-        mvc.perform(get(INTERFACES_URL + serviceInterface.getServiceInterfaceName()).accept(MediaTypes.HAL_JSON_VALUE))
+        mvc.perform(get(INTERFACES_URL + "/" + serviceInterface.getServiceInterfaceName()).accept(MediaTypes.HAL_JSON_VALUE))
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(getServiceInterfaceMatcher(serviceInterface, INTERFACES_URL, SERVICE_URL))
@@ -137,7 +138,7 @@ public class ServiceInterfaceControllerTests {
         given(serviceRepository.findInterfaceOfServiceByName(any(), any(), any())).willReturn(
             Optional.empty());
 
-        mvc.perform(get(INTERFACES_URL + "NotThereInterface").accept(MediaTypes.HAL_JSON_VALUE))
+        mvc.perform(get(INTERFACES_URL + "/NotThereInterface").accept(MediaTypes.HAL_JSON_VALUE))
             .andDo(print())
             .andExpect(status().isNotFound())
             .andReturn();
@@ -160,6 +161,54 @@ public class ServiceInterfaceControllerTests {
             .andReturn();
     }
 
+    @Test
+    public void getInterfacePublicIpByName() throws Exception {
+
+        List<String> externalIPs = new ArrayList<>();
+        externalIPs.add("1.2.3.4");
+        MicoServiceInterface serviceInterface = getTestServiceInterface();
+        given(serviceRepository.findInterfaceOfServiceByName(serviceInterface.getServiceInterfaceName(), SHORT_NAME, VERSION)).willReturn(
+            Optional.of(serviceInterface));
+        Service service = getKubernetesService(serviceInterface.getServiceInterfaceName(), externalIPs);
+        given(cluster.getService(serviceInterface.getServiceInterfaceName(),
+            kubernetesConfig.getNamespaceMicoWorkspace())).willReturn(service);
+
+        mvc.perform(get(INTERFACES_URL + "/" + serviceInterface.getServiceInterfaceName() + "/" + PATH_PART_PUBLIC_IP).accept(MediaTypes.HAL_JSON_VALUE))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(1)))
+            .andExpect(jsonPath("$", is(externalIPs)))
+            .andReturn();
+    }
+
+    @Test
+    public void getInterfacePublicIpByNameWithPendingIP() throws Exception {
+
+        List<String> externalIPs = new ArrayList<>();
+        MicoServiceInterface serviceInterface = getTestServiceInterface();
+        given(serviceRepository.findInterfaceOfServiceByName(serviceInterface.getServiceInterfaceName(), SHORT_NAME, VERSION)).willReturn(
+            Optional.of(serviceInterface));
+        Service service = getKubernetesService(serviceInterface.getServiceInterfaceName(), externalIPs);
+        given(cluster.getService(serviceInterface.getServiceInterfaceName(),
+            kubernetesConfig.getNamespaceMicoWorkspace())).willReturn(service);
+
+        mvc.perform(get(INTERFACES_URL + "/" + serviceInterface.getServiceInterfaceName() + "/" + PATH_PART_PUBLIC_IP).accept(MediaTypes.HAL_JSON_VALUE))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().json("[]"))
+            .andReturn();
+    }
+
+    private Service getKubernetesService(String serviceInterfaceName, List<String> externalIPs) {
+        return new ServiceBuilder()
+            .withNewMetadata()
+            .withName(serviceInterfaceName)
+            .endMetadata()
+            .withNewSpec()
+            .withExternalIPs(externalIPs)
+            .endSpec()
+            .build();
+    }
 
     private MicoServiceInterface getTestServiceInterface() {
         return MicoServiceInterface.builder()
@@ -174,8 +223,8 @@ public class ServiceInterfaceControllerTests {
             .build();
     }
 
-    private ResultMatcher getServiceInterfaceMatcher(MicoServiceInterface serviceInterface, String selfBaseUrl, String serviceUrl) throws UnsupportedEncodingException, URISyntaxException, MalformedURLException {
-        URI selfHrefEnding = UriComponentsBuilder.fromUriString(selfBaseUrl + serviceInterface.getServiceInterfaceName()).build().encode().toUri();
+    private ResultMatcher getServiceInterfaceMatcher(MicoServiceInterface serviceInterface, String selfBaseUrl, String serviceUrl) {
+        URI selfHrefEnding = UriComponentsBuilder.fromUriString(selfBaseUrl + "/" + serviceInterface.getServiceInterfaceName()).build().encode().toUri();
         return ResultMatcher.matchAll(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE),
             jsonPath("$.serviceInterfaceName", is(serviceInterface.getServiceInterfaceName())),
             jsonPath("$.ports", hasSize(serviceInterface.getPorts().size())),
