8674a8096be55e42942f8db7087fffdd668497e7
Heiko Geppert <heiko.geppert@outlook.de>
2019-02-04 12:49:36 +0100

    Fix application creation with services

--- mico-admin/src/app/dashboard/dashboard.component.ts
+++ mico-admin/src/app/dashboard/dashboard.component.ts
@@ -105,7 +105,7 @@ export class DashboardComponent implements OnInit {
                 }
 
                 const data = result.applicationProperties;
-                data.dependsOn = result.services;
+                data.services = result.services;
 
                 this.apiService.postApplication(data).subscribe(val => {
                     this.router.navigate(['app-detail', val.shortName, val.version]);
