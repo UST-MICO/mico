2357987cf8c2bd91770864045b0517d252e3da38
Heiko Geppert <heiko.geppert@gmx.de>
2019-02-18 16:47:11 +0100

    Add array handling for data-views

--- mico-admin/src/app/data-view/mico-data-array/mico-data-array.component.ts
+++ mico-admin/src/app/data-view/mico-data-array/mico-data-array.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ApiModel } from 'src/app/api/apimodel';
 
 @Component({
-  selector: 'mico-mico-data-array',
-  templateUrl: './mico-data-array.component.html',
-  styleUrls: ['./mico-data-array.component.css']
+    selector: 'mico-data-array',
+    templateUrl: './mico-data-array.component.html',
+    styleUrls: ['./mico-data-array.component.css']
 })
 export class MicoDataArrayComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    @Input() config: ApiModel;
+    @Input() dataValue: any;
 
+    ngOnInit() {
+    }
 }
