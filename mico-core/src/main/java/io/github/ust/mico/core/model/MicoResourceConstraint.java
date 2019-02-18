9317176f4be32ca6b3505b060293fc0cdd806345
Heiko Geppert <heiko.geppert@gmx.de>
2019-02-18 11:54:51 +0100

    Add annotations for MicoResourceConstraint

--- mico-core/src/main/java/io/github/ust/mico/core/model/MicoResourceConstraint.java
+++ mico-core/src/main/java/io/github/ust/mico/core/model/MicoResourceConstraint.java
@@ -1,5 +1,9 @@
 package io.github.ust.mico.core.model;
 
+import io.github.ust.mico.core.configuration.extension.CustomOpenApiExtentionsPlugin;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.Extension;
+import io.swagger.annotations.ExtensionProperty;
 import lombok.Value;
 
 /**
@@ -19,11 +23,27 @@ public class MicoResourceConstraint {
      * - 1 Hyperthread on a bare-metal Intel processor with Hyperthreading
      * Can also be specified as a fraction up to precision 0.001.
      */
+    @ApiModelProperty(extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Number Of CPU Units"),
+            @ExtensionProperty(name = "x-order", value = "10"),
+            @ExtensionProperty(name = "description", value = "Measured in CPU units. One Kubernetes CPU (unit) is equivaletnt to:\n- 1 AWS vCPU\n- 1 GCP Core\n- 1 Azure vCore\n- 1 IBM vCPU\n- 1 Hyperthread on a bare-metal Intel processor with Hyperthreading\n Can also be specified as a fraction up to precision 0.001.")
+        }
+    )})
     private final double cpuUnits;
 
     /**
      * Memory in bytes.
      */
+    @ApiModelProperty(extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Memory"),
+            @ExtensionProperty(name = "x-order", value = "20"),
+            @ExtensionProperty(name = "description", value = "Memory in bytes")
+        }
+    )})
     private final long memoryInBytes;
 
 }
