09cbfbb493d7758fdd7c9af707d08c7441f7a721
Heiko Geppert <heiko.geppert@gmx.de>
2019-02-18 11:49:05 +0100

    Add annotations for MicoImageContainer

--- mico-core/src/main/java/io/github/ust/mico/core/model/MicoImageContainer.java
+++ mico-core/src/main/java/io/github/ust/mico/core/model/MicoImageContainer.java
@@ -3,7 +3,10 @@ package io.github.ust.mico.core.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.github.ust.mico.core.configuration.extension.CustomOpenApiExtentionsPlugin;
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.Extension;
+import io.swagger.annotations.ExtensionProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,13 +31,27 @@ public class MicoImageContainer {
      * The name of the docker image.
      * Defaults to Service#shortName.
      */
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(required = true, extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Image Name"),
+            @ExtensionProperty(name = "x-order", value = "10"),
+            @ExtensionProperty(name = "description", value = "The name of the docker image")
+        }
+    )})
     private String image;
 
     /**
      * The list of ports for this service.
      */
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(required = true, extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Ports"),
+            @ExtensionProperty(name = "x-order", value = "20"),
+            @ExtensionProperty(name = "description", value = "The list of ports for this service")
+        }
+    )})
     private List<MicoPort> ports = new ArrayList<>();
 
 
@@ -46,7 +63,14 @@ public class MicoImageContainer {
      * The name of the container (in the Kubernetes Pod).
      * Defaults to Service#shortName.
      */
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(required = true, extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Container Name"),
+            @ExtensionProperty(name = "x-order", value = "30"),
+            @ExtensionProperty(name = "description", value = "The name of the container (in the Kubernetes Pod)")
+        }
+    )})
     private String name;
 
     /**
@@ -54,18 +78,42 @@ public class MicoImageContainer {
      * resources allowed. If omitted it defaults to the
      * upper limit if that is explicitly specified.
      */
+    @ApiModelProperty(extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Resource Limit (minimum)"),
+            @ExtensionProperty(name = "x-order", value = "40"),
+            @ExtensionProperty(name = "description", value = "Limit describing the minimum amount of compute resources allowed. If omitted it defaults to the upper limit if that is explicitly specified")
+        }
+    )})
     private MicoResourceConstraint resourceLowerLimit;
 
     /**
      * Limit describing the maximum amount of compute
      * resources allowed.
      */
+    @ApiModelProperty(extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Resource Limit (maximum)"),
+            @ExtensionProperty(name = "x-order", value = "50"),
+            @ExtensionProperty(name = "description", value = "Limit describing the maximum amount of compute resources allowed.")
+        }
+    )})
     private MicoResourceConstraint resourceUpperLimit;
 
     /**
      * Indicates whether this container should have
      * a read-only root file system. Defaults to false.
      */
+    @ApiModelProperty(extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Read Only Root File System"),
+            @ExtensionProperty(name = "x-order", value = "60"),
+            @ExtensionProperty(name = "description", value = "Indicates whether this container should have a read-only root file system. Defaults to false.")
+        }
+    )})
     private boolean readOnlyRootFileSystem = false;
 
     /**
@@ -73,6 +121,15 @@ public class MicoImageContainer {
      * If somehow not run as non-root user (not UID 0) it will
      * fail to start. Default to false.
      */
+    @ApiModelProperty(extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Run As Non Root"),
+            @ExtensionProperty(name = "x-order", value = "70"),
+            @ExtensionProperty(name = "description", value = "Indicates whether the service must run as a non-root user.\n" +
+                "     * If somehow not run as non-root user (not UID 0) it will fail to start. Default to false.")
+        }
+    )})
     private boolean runAsNonRoot = false;
 
 }
