d6260fcc27b4ef7253302cfb44cc4ace5573af00
Heiko Geppert <heiko.geppert@gmx.de>
2019-02-18 11:18:06 +0100

    Add annotations for MicoPort

--- mico-core/src/main/java/io/github/ust/mico/core/model/MicoPort.java
+++ mico-core/src/main/java/io/github/ust/mico/core/model/MicoPort.java
@@ -1,6 +1,9 @@
 package io.github.ust.mico.core.model;
 
+import io.github.ust.mico.core.configuration.extension.CustomOpenApiExtentionsPlugin;
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.Extension;
+import io.swagger.annotations.ExtensionProperty;
 import lombok.Value;
 
 /**
@@ -16,13 +19,27 @@ public class MicoPort {
     /**
      * The port number.
      */
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(required = true, extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Port Number"),
+            @ExtensionProperty(name = "x-order", value = "10"),
+            @ExtensionProperty(name = "description", value = "The port number")
+        }
+    )})
     private final int number;
 
     /**
      * The type (protocol) of the port.
      */
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(required = true, extensions = {@Extension(
+        name = CustomOpenApiExtentionsPlugin.X_MICO_CUSTOM_EXTENSION,
+        properties = {
+            @ExtensionProperty(name = "title", value = "Port Type"),
+            @ExtensionProperty(name = "x-order", value = "20"),
+            @ExtensionProperty(name = "description", value = "The type (protocol) of the port")
+        }
+    )})
     private final MicoPortType type = MicoPortType.DEFAULT;
 
 }
