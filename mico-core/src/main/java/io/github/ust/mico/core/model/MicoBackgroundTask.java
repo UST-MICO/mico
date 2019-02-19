83e5e4997702ea0ef330809446d1c94244de2c95
Joachim Spalink <j.spalink@gmx.de>
2019-02-19 10:28:49 +0100

    start data model

--- mico-core/src/main/java/io/github/ust/mico/core/model/MicoBackgroundTask.java
+++ mico-core/src/main/java/io/github/ust/mico/core/model/MicoBackgroundTask.java
@@ -1,21 +1,27 @@
 package io.github.ust.mico.core.model;
 
-import java.util.concurrent.CompletableFuture;
+import org.springframework.data.redis.core.RedisHash;
 
+import java.util.concurrent.CompletableFuture;
+@RedisHash("BackgroundJob")
 public class MicoBackgroundTask {
-    enum TYPE{
+    enum Type{
         IMPORT, BUILD
     }
-    enum STATUS{
+    enum Status{
         PENDING, RUNNING, CANCELLED, ERROR, DONE
     }
+
     Long id;
     CompletableFuture job;
     MicoService service;
+    Status status;
+    Type type;
     public MicoBackgroundTask(CompletableFuture job, MicoService service){
         this.job = job;
         this.service =service;
     }
+
     public void cancelJob(){
         job.cancel(false);
     }
--- mico-core/src/main/java/io/github/ust/mico/core/persistence/MicoBackgroundTaskRepository.java
+++ mico-core/src/main/java/io/github/ust/mico/core/persistence/MicoBackgroundTaskRepository.java
@@ -2,6 +2,9 @@ package io.github.ust.mico.core.persistence;
 
 import io.github.ust.mico.core.model.MicoBackgroundTask;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MicoBackgroundTaskRepository extends Neo4jRepository<MicoBackgroundTask,Long> {
+@Repository
+public interface MicoBackgroundTaskRepository extends CrudRepository<MicoBackgroundTask, Long> {
 }
