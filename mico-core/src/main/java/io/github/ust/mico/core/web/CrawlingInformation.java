f7d995ddb64229fabc576efe93fe5c7325c992f7
JanVetter <25137666+JanVetter@users.noreply.github.com>
2019-02-22 13:34:11 +0100

    Add comments

--- mico-core/src/main/java/io/github/ust/mico/core/web/CrawlingInformation.java
+++ mico-core/src/main/java/io/github/ust/mico/core/web/CrawlingInformation.java
@@ -3,13 +3,25 @@ package io.github.ust.mico.core.web;
 import lombok.Getter;
 
 
+/**
+ * This class is used for providing information to the GitHub crawler via the API endpoints.
+ */
 public class CrawlingInformation {
 
+    /**
+     * Required.
+     *
+     * Link to the GitHub repo to crawl from
+     */
     @Getter
     private String uri;
 
     /**
-     * Can be 'latest' or a specific version (e.g. v1.2.3)
+     * Optional.
+     *
+     * Can be one of the following:
+     * - 'latest'
+     * - specific version (e.g., 'v1.0.0')
      */
     @Getter
     private String version = "";
