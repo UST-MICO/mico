ac1d497e1f5c9b7a074d4b912eeb4c675cfffccd
Heiko Geppert <heiko.geppert@gmx.de>
2019-02-25 13:38:24 +0100

    Fix get dependees

--- mico-admin/src/app/api/api.service.ts
+++ mico-admin/src/app/api/api.service.ts
@@ -568,7 +568,7 @@ export class ApiService {
 
         this.rest.get<ApiObject>(resource).subscribe(val => {
             if (val.hasOwnProperty('_embedded')) {
-                stream.next(freezeObject(val._embedded.dependencies));
+                stream.next(freezeObject(val._embedded.micoServiceList));
             } else {
                 stream.next(freezeObject([]));
             }
