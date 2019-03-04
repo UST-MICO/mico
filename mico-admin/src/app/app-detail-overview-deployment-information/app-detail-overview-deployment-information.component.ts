624e3f86346bd107e6fca764344c0e0b834ef31a
Heiko Geppert <heiko.geppert@gmx.de>
2019-03-04 12:04:06 +0100

    Fix deploymentInformation call

--- mico-admin/src/app/app-detail-overview-deployment-information/app-detail-overview-deployment-information.component.ts
+++ mico-admin/src/app/app-detail-overview-deployment-information/app-detail-overview-deployment-information.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { ApiService } from '../api/api.service';
 
 @Component({
     selector: 'mico-app-detail-overview-deployment-information',
     templateUrl: './app-detail-overview-deployment-information.component.html',
     styleUrls: ['./app-detail-overview-deployment-information.component.css']
 })
-export class AppDetailOverviewDeploymentInformationComponent implements OnInit {
+export class AppDetailOverviewDeploymentInformationComponent implements OnChanges {
 
-    constructor() { }
+    constructor(private apiService: ApiService) { }
 
     @Input() service;
     @Input() applicationShortName: string;
     @Input() applicationVersion: string;
 
-    ngOnInit() {
+    ngOnChanges() {
+        if (this.service != null && this.applicationShortName != null && this.applicationVersion != null) {
+
+            this.apiService.getServiceDeploymentInformation(this.applicationShortName, this.applicationVersion, this.service.shortName)
+                .subscribe(val => {
+                    console.log(val);
+                });
+        }
     }
 
 }
