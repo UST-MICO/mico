895e1cb264cdbd344a97d92531791c86fb2bc216
Marvin Wohlfarth <st112462@stud.uni-stuttgart.de>
2019-03-05 14:22:27 +0100

    Add KubernetesNodeMetricsDTO to avoid Maps with metrics in MicoServiceStatusDTO (#531)

--- mico-core/src/test/java/io/github/ust/mico/core/TestConstants.java
+++ mico-core/src/test/java/io/github/ust/mico/core/TestConstants.java
@@ -23,7 +23,9 @@ import io.github.ust.mico.core.dto.MicoApplicationStatusDTO;
 import io.github.ust.mico.core.dto.MicoServiceStatusDTO;
 import io.github.ust.mico.core.model.MicoVersion;
 
-import static io.github.ust.mico.core.JsonPathBuilder.*;
+import static io.github.ust.mico.core.JsonPathBuilder.ROOT;
+import static io.github.ust.mico.core.JsonPathBuilder.buildAttributePath;
+import static io.github.ust.mico.core.JsonPathBuilder.buildPath;
 
 public class TestConstants {
 
@@ -93,14 +95,17 @@ public class TestConstants {
      * All paths are build on the path for the status of this service.
      */
     /**
-     * Path of a single {@link MicoServiceStatusDTO} in a {@link MicoApplicationStatusDTO}. Contains status information for this service.
+     * Path of a single {@link MicoServiceStatusDTO} in a {@link MicoApplicationStatusDTO}. Contains status information
+     * for this service.
      */
     public static final String SERVICE_STATUS_PATH = buildPath(ROOT, "serviceStatuses[0]");
     public static final String SERVICE_INFORMATION_NAME = buildPath(SERVICE_STATUS_PATH, "name");
     public static final String REQUESTED_REPLICAS = buildPath(SERVICE_STATUS_PATH, "requestedReplicas");
     public static final String AVAILABLE_REPLICAS = buildPath(SERVICE_STATUS_PATH, "availableReplicas");
-    public static final String AVERAGE_CPU_LOAD_PER_NODE_PATH = buildPath(SERVICE_STATUS_PATH, "averageCpuLoadPerNode");
-    public static final String AVERAGE_MEMORY_USAGE_PER_NODE_PATH = buildPath(SERVICE_STATUS_PATH, "averageMemoryUsagePerNode");
+    public static final String NODE_METRICS_PATH_FIRST_ELEMENT = buildPath(SERVICE_STATUS_PATH, "nodeMetrics[0]");
+    public static final String NODE_METRICS_NAME = buildPath(NODE_METRICS_PATH_FIRST_ELEMENT, "nodeName");
+    public static final String NODE_METRICS_AVERAGE_CPU_LOAD = buildPath(NODE_METRICS_PATH_FIRST_ELEMENT, "averageCpuLoad");
+    public static final String NODE_METRICS_AVERAGE_MEMORY_USAGE = buildPath(NODE_METRICS_PATH_FIRST_ELEMENT, "averageMemoryUsage");
     public static final String ERROR_MESSAGES = buildPath(SERVICE_STATUS_PATH, "errorMessages");
     public static final String INTERFACES_INFORMATION = buildPath(SERVICE_STATUS_PATH, "interfacesInformation");
     public static final String INTERFACES_INFORMATION_NAME = buildPath(SERVICE_STATUS_PATH, "interfacesInformation[0].name");
@@ -130,8 +135,10 @@ public class TestConstants {
     public static final String SERVICE_DTO_SERVICE_NAME = buildAttributePath("name");
     public static final String SERVICE_DTO_REQUESTED_REPLICAS = buildPath(ROOT, "requestedReplicas");
     public static final String SERVICE_DTO_AVAILABLE_REPLICAS = buildPath(ROOT, "availableReplicas");
-    public static final String SERVICE_DTO_AVERAGE_CPU_LOAD_PER_NODE = buildPath(ROOT, "averageCpuLoadPerNode");
-    public static final String SERVICE_DTO_AVERAGE_MEMORY_USAGE_PER_NODE = buildPath(ROOT, "averageMemoryUsagePerNode");
+    public static final String SERVICE_DTO_NODE_METRICS = buildPath(ROOT, "nodeMetrics[0]");
+    public static final String SERVICE_DTO_NODE_NAME = buildPath(SERVICE_DTO_NODE_METRICS, "nodeName");
+    public static final String SERVICE_DTO_NODE_METRICS_AVERAGE_CPU_LOAD = buildPath(SERVICE_DTO_NODE_METRICS, "averageCpuLoad");
+    public static final String SERVICE_DTO_NODE_METRICS_AVERAGE_MEMORY_USAGE = buildPath(SERVICE_DTO_NODE_METRICS, "averageMemoryUsage");
     public static final String SERVICE_DTO_ERROR_MESSAGES = buildPath(ROOT, "errorMessages");
     public static final String SERVICE_DTO_INTERFACES_INFORMATION = buildPath(ROOT, "interfacesInformation");
     public static final String SERVICE_DTO_INTERFACES_INFORMATION_NAME = buildPath(ROOT, "interfacesInformation[0].name");
