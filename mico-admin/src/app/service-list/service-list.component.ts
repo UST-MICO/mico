160d2501efff10840110b153b35d1a04c8976c65
Heiko Geppert <heiko.geppert@gmx.de>
2019-03-07 17:28:07 +0100

    Add further add application/service button to the corresponding list views

--- mico-admin/src/app/service-list/service-list.component.ts
+++ mico-admin/src/app/service-list/service-list.component.ts
@@ -23,9 +23,11 @@ import { Subscription } from 'rxjs';
 import { from } from 'rxjs';
 import { groupBy, mergeMap, toArray, map } from 'rxjs/operators';
 import { ApiObject } from '../api/apiobject';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSnackBar } from '@angular/material';
 import { YesNoDialogComponent } from '../dialogs/yes-no-dialog/yes-no-dialog.component';
 import { UtilsService } from '../util/utils.service';
+import { Router } from '@angular/router';
+import { CreateServiceDialogComponent } from '../dialogs/create-service/create-service.component';
 
 
 @Component({
@@ -41,6 +43,8 @@ export class ServiceListComponent implements OnInit, OnDestroy {
         private apiService: ApiService,
         private dialog: MatDialog,
         private util: UtilsService,
+        private router: Router,
+        private snackBar: MatSnackBar,
     ) {
         this.getServices();
     }
@@ -103,4 +107,49 @@ export class ServiceListComponent implements OnInit, OnDestroy {
         });
     }
 
+
+    /**
+     * dialog to create a new service. can be done:
+     * - manually
+     * - via github import
+     * uses: POST services or POST services/import/github
+     */
+    newService(): void {
+        const dialogRef = this.dialog.open(CreateServiceDialogComponent);
+
+        const subDialog = dialogRef.afterClosed().subscribe(result => {
+
+            // filter empty results (when dialog is aborted)
+            if (!result) {
+                return;
+            }
+
+            // check if returned object is complete
+            for (const property in result.data) {
+                if (result.data[property] == null) {
+
+                    if (property !== 'serviceInterfaces') {
+                        this.snackBar.open('Missing property: ' + property, 'Ok', {
+                            duration: 8000,
+                        });
+                        return;
+                    }
+                }
+            }
+
+            // decide if the service was created manually or is to be created via github crawler and create service
+            if (result.tab === 'manual') {
+                this.apiService.postService(result.data).subscribe(val => {
+                    this.router.navigate(['service-detail', val.shortName, val.version]);
+                });
+            } else if (result.tab === 'github') {
+
+                this.apiService.postServiceViaGithub(result.data.url, result.data.version).subscribe(val => {
+                    this.router.navigate(['service-detail', val.shortName, val.version]);
+                });
+            }
+
+            this.util.safeUnsubscribe(subDialog);
+        });
+    }
 }
