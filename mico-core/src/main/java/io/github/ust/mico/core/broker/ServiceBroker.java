8a15a9aa24de2fa2ebe4628ee80aff8c61a4a3ee
Pascal157600 <st157600@stud.uni-stuttgart.de>
2019-03-10 12:16:27 +0100

    Add getServiceFromDatabase method to broker class

--- mico-core/src/main/java/io/github/ust/mico/core/broker/ServiceBroker.java
+++ mico-core/src/main/java/io/github/ust/mico/core/broker/ServiceBroker.java
@@ -3,9 +3,12 @@ package io.github.ust.mico.core.broker;
 import io.github.ust.mico.core.model.MicoService;
 import io.github.ust.mico.core.persistence.MicoServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ServiceBroker {
@@ -16,4 +19,16 @@ public class ServiceBroker {
     public List<MicoService> getAllServicesAsList (){
         return serviceRepository.findAll(2);
     }
+
+    public MicoService getServiceFromDatabase(String shortName, String version) throws ResponseStatusException {
+        Optional<MicoService> serviceOpt = serviceRepository.findByShortNameAndVersion(shortName, version);
+        if (!serviceOpt.isPresent()) {
+            // TODO: Verfiy if we want to throw http response exceptions inside the broker classes
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Service '" + shortName + "' '" + version + "' was not found!");
+        }
+        return serviceOpt.get();
+    }
+
+    
+
 }
