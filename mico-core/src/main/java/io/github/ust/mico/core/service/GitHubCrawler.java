c4ea23bd010eb471ac562981bd1f221761b474aa
Tobias Schneider <schneider.tobias.93@web.de>
2019-03-11 20:20:12 +0100

    Revert "Catch correct exception"
    This reverts commit 4a8fe1366aff6c1d8d39722d0b5508da8515d1e3.

--- mico-core/src/main/java/io/github/ust/mico/core/service/GitHubCrawler.java
+++ mico-core/src/main/java/io/github/ust/mico/core/service/GitHubCrawler.java
@@ -31,7 +31,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -67,13 +66,13 @@ public class GitHubCrawler {
 
         try {
             responseBasicInfo = restTemplate.getForEntity(uriBasicInfo, String.class);
-        } catch (HttpClientErrorException e) {
+        } catch (Exception e) {
             throw new IllegalArgumentException("GitHub repository " + uriBasicInfo.replace(GITHUB_API_URL, "") + " does not exist!");
         }
 
         try {
             responseReleaseInfo = restTemplate.getForEntity(uriReleaseInfo, String.class);
-        } catch (HttpClientErrorException e) {
+        } catch (Exception e) {
             throw new IllegalArgumentException("GitHub repository " + uriBasicInfo.replace(GITHUB_API_URL, "")
                 + " doesn't have a release " + uriReleaseInfo.replace(GITHUB_API_URL, "") + "!");
         }
