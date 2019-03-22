7688bcbb3d879815983a9723df19ed223a5547c1
Heiko Geppert <heiko.geppert@gmx.de>
2019-03-22 19:03:30 +0100

    Add ui change requests

--- mico-admin/src/app/service-detail-kubeconfig/service-detail-kubeconfig.component.ts
+++ mico-admin/src/app/service-detail-kubeconfig/service-detail-kubeconfig.component.ts
@@ -1,6 +1,26 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import { Component, OnChanges, Input, OnDestroy } from '@angular/core';
 import { ApiService } from '../api/api.service';
 import { Subscription } from 'rxjs';
+import { safeUnsubscribe } from '../util/utils';
 
 @Component({
     selector: 'mico-service-detail-kubeconfig',
@@ -23,8 +43,9 @@ export class ServiceDetailKubeconfigComponent implements OnChanges, OnDestroy {
     ngOnChanges() {
         if (this.shortName != null && this.version != null) {
 
-            // TODO safeUnsubscribe subYaml
+            // get the yaml string
 
+            safeUnsubscribe(this.subYaml);
             this.subYaml = this.apiService.getServiceYamlConfig(this.shortName, this.version)
                 .subscribe(val => {
                     console.log(val);
@@ -33,7 +54,10 @@ export class ServiceDetailKubeconfigComponent implements OnChanges, OnDestroy {
         }
     }
 
+    /**
+     * unsubscribe safely from all subscriptions
+     */
     ngOnDestroy() {
-        // TODO unsubscribe as soon as ui-maintenance-3 branch is merged
+        safeUnsubscribe(this.subYaml);
     }
 }
