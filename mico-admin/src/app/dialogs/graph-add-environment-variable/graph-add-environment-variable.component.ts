3756f97b7ea278e9c20c4d600b2411e864348ffc
Heiko Geppert <heiko.geppert@gmx.de>
2019-03-25 17:46:00 +0100

    Add missing elements for add environmentVariable dialog

--- mico-admin/src/app/dialogs/graph-add-environment-variable/graph-add-environment-variable.component.ts
+++ mico-admin/src/app/dialogs/graph-add-environment-variable/graph-add-environment-variable.component.ts
@@ -1,44 +1,32 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { ApiService } from 'src/app/api/api.service';
-import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'mico-graph-add-environment-variable',
     templateUrl: './graph-add-environment-variable.component.html',
     styleUrls: ['./graph-add-environment-variable.component.css']
 })
-export class GraphAddEnvironmentVariableComponent implements OnInit {
+export class GraphAddEnvironmentVariableComponent {
+
 
     constructor(public dialogRef: MatDialogRef<GraphAddEnvironmentVariableComponent>,
         @Inject(MAT_DIALOG_DATA) public data: any,
-        private apiService: ApiService,
     ) {
 
-        this.applicationShortName = data.applicationShortName;
-        this.applicationVersion = data.applicationVersion;
         this.serviceShortName = data.serviceShortName;
         this.interfaceName = data.interfaceName;
     }
 
-    applicationShortName;
-    applicationVersion;
     serviceShortName;
     interfaceName;
 
-    deploymentInformation;
-
     chosenEnvVar;
 
-    // TODO unsubsribe
-    subDeploymentInformation: Subscription;
-
-    ngOnInit() {
-        this.subDeploymentInformation = this.apiService.getServiceDeploymentInformation(this.applicationShortName,
-            this.applicationVersion, this.serviceShortName)
-            .subscribe(val => {
-                this.deploymentInformation = val;
-            });
+    /**
+     * return method of the dialog
+     */
+    response() {
+        return this.chosenEnvVar;
     }
 
 }
