d0f7cc9f888897febde1cfac092e8c25ca5305c7
Pascal157600 <st157600@stud.uni-stuttgart.de>
2019-03-25 11:12:17 +0100

    Inital deployment broker update

--- mico-core/src/main/java/io/github/ust/mico/core/broker/DeploymentBroker.java
+++ mico-core/src/main/java/io/github/ust/mico/core/broker/DeploymentBroker.java
@@ -1,4 +1,10 @@
 package io.github.ust.mico.core.broker;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class DeploymentBroker {
+    
 }
