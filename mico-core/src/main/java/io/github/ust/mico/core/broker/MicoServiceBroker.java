b619336e5e7683301b7c12dec08eb9538d1c9bd6
David Kopp <david@kopp-heim.de>
2019-03-28 00:13:18 +0100

    Fix promote service should remove docker image uri

--- mico-core/src/main/java/io/github/ust/mico/core/broker/MicoServiceBroker.java
+++ mico-core/src/main/java/io/github/ust/mico/core/broker/MicoServiceBroker.java
@@ -233,21 +233,22 @@ public class MicoServiceBroker {
     }
 
     public MicoService promoteService(MicoService service, String newVersion) throws MicoServiceAlreadyExistsException {
+
+        // Update the version of the application
+        service.setVersion(newVersion);
+
+        // Remove docker image URI
+        service.setDockerImageUri(null);
+
         // In order to copy the service along with all service interfaces nodes
         // and all port nodes of the service interface we need to set the id of
         // service interfaces and ports to null.
         // That way, Neo4j will create new entities instead of updating the existing ones.
-        service.setVersion(newVersion).setId(null);
+        service.setId(null);
         service.getServiceInterfaces().forEach(serviceInterface -> serviceInterface.setId(null));
         service.getServiceInterfaces().forEach(serviceInterface -> serviceInterface.getPorts().forEach(port -> port.setId(null)));
 
-        log.debug("Set new version in service: {}", service);
-
-        MicoService updatedService = persistService(service);
-
-        log.debug("Updated service: {}", updatedService);
-
-        return updatedService;
+        return persistService(service);
     }
 
     //TODO: Create test
