76c3461049c7040f6566e0b60b61f52a85574df7
David Kopp <david@kopp-heim.de>
2019-08-20 13:39:59 +0200

    Fix topic repository tests
    Definition of the active profile was missing. We need the 'local' profile so that no Kubernetes connection will be established.

--- mico-core/src/test/java/io/github/ust/mico/core/MicoTopicRepositoryTests.java
+++ mico-core/src/test/java/io/github/ust/mico/core/MicoTopicRepositoryTests.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
+@ActiveProfiles("local")
 public class MicoTopicRepositoryTests extends MicoRepositoryTests {
 
 
