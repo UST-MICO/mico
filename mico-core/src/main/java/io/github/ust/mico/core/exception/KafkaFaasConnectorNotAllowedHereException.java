f155c586927a36b9ccc523ab6b19d6b1d9d4a0d6
David Kopp <david@kopp-heim.de>
2019-08-27 10:32:36 +0200

    Fix error message for KF_Connector not allowed here

--- mico-core/src/main/java/io/github/ust/mico/core/exception/KafkaFaasConnectorNotAllowedHereException.java
+++ mico-core/src/main/java/io/github/ust/mico/core/exception/KafkaFaasConnectorNotAllowedHereException.java
@@ -5,6 +5,7 @@ public class KafkaFaasConnectorNotAllowedHereException extends Exception {
     private static final long serialVersionUID = -6352481291495405718L;
 
     public KafkaFaasConnectorNotAllowedHereException() {
-        super("KafkaFaasConnector is not allowed here. Use special API endpoints instead.");
+        super("KafkaFaasConnector is a special MICO service that is handled differently. " +
+            "Therefore there are other API endpoints for handling KafkaFaasConnectors.");
     }
 }
