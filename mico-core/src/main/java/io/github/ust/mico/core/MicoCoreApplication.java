c57e96556f8733c9e7c5c76f205f5db50c7dd437
Joachim Spalink <j.spalink@gmx.de>
2019-08-27 11:37:33 +0200

    Add profile for travis

--- mico-core/src/main/java/io/github/ust/mico/core/MicoCoreApplication.java
+++ mico-core/src/main/java/io/github/ust/mico/core/MicoCoreApplication.java
@@ -88,7 +88,7 @@ public class MicoCoreApplication implements ApplicationListener<ApplicationReady
      */
     public void onApplicationEvent(final ApplicationReadyEvent event) {
         Environment environment = event.getApplicationContext().getEnvironment();
-        if (environment.acceptsProfiles(Profiles.of("local"))) {
+        if (environment.acceptsProfiles(Profiles.of("travis"))) {
             log.info("Local profile is active. Don't add kafka-faas-connector to database.");
             return;
         }
