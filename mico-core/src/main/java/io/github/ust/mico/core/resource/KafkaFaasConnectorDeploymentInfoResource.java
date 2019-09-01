ea98748b6cd640f992bb7578676c65b47d90256d
David Kopp <david@kopp-heim.de>
2019-09-01 10:54:29 +0200

    Minor changes

--- mico-core/src/main/java/io/github/ust/mico/core/resource/KafkaFaasConnectorDeploymentInfoResource.java
+++ mico-core/src/main/java/io/github/ust/mico/core/resource/KafkaFaasConnectorDeploymentInfoResource.java
@@ -50,15 +50,15 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @RequestMapping(value = PATH_APPLICATIONS, produces = MediaTypes.HAL_JSON_VALUE)
 public class KafkaFaasConnectorDeploymentInfoResource {
 
-    public static final String PATH_KAFKA_FAAS_CONNECOTR_DEPLOYMENT_INFORMATION = "kafkaFaasConnectorDeploymentInformation";
+    public static final String PATH_KAFKA_FAAS_CONNECTOR_DEPLOYMENT_INFORMATION = "kafkaFaasConnectorDeploymentInformation";
 
     @Autowired
     private KafkaFaasConnectorDeploymentInfoBroker kafkaFaasConnectorDeploymentInfoBroker;
 
-    @GetMapping("/{" + PATH_VARIABLE_SHORT_NAME + "}/{" + PATH_VARIABLE_VERSION + "}/" + PATH_KAFKA_FAAS_CONNECOTR_DEPLOYMENT_INFORMATION)
+    @GetMapping("/{" + PATH_VARIABLE_SHORT_NAME + "}/{" + PATH_VARIABLE_VERSION + "}/" + PATH_KAFKA_FAAS_CONNECTOR_DEPLOYMENT_INFORMATION)
     public ResponseEntity<Resources<Resource<MicoServiceDeploymentInfoResponseDTO>>> getKafkaFaasConnectorDeploymentInformation(@PathVariable(PATH_VARIABLE_SHORT_NAME) String shortName,
                                                                                                                                 @PathVariable(PATH_VARIABLE_VERSION) String version) {
-        log.debug("Request to getKafkaFaasConnectorDeploymentInformation with the micoApplicationShortname '{}' and version '{}'.", shortName, version);
+        log.debug("Get the KafkaFaasConnector deployment information of the MicoApplication '{}' '{}'.", shortName, version);
         List<MicoServiceDeploymentInfo> micoServiceDeploymentInfos;
         try {
             micoServiceDeploymentInfos = kafkaFaasConnectorDeploymentInfoBroker.getKafkaFaasConnectorDeploymentInformation(shortName, version);
@@ -76,8 +76,8 @@ public class KafkaFaasConnectorDeploymentInfoResource {
             Resource<MicoServiceDeploymentInfoResponseDTO> micoServiceDeploymentInfoResource = new Resource<>(serviceDeploymentInfoResponseDto, links);
             micoServiceDeploymentInfoResources.add(micoServiceDeploymentInfoResource);
         }
-        return ResponseEntity.ok(new Resources<>(micoServiceDeploymentInfoResources, linkTo(methodOn(KafkaFaasConnectorDeploymentInfoResource.class).getKafkaFaasConnectorDeploymentInformation(shortName, version)).withSelfRel()));
+        return ResponseEntity.ok(new Resources<>(micoServiceDeploymentInfoResources,
+            linkTo(methodOn(KafkaFaasConnectorDeploymentInfoResource.class).getKafkaFaasConnectorDeploymentInformation(shortName, version)).withSelfRel()));
     }
-
 
 }
